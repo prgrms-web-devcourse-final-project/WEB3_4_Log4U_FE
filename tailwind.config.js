d589885b93b70bfcf5bd2452697f3cca3ad81ab4
JiNookk <ojw0828@naver.com>
2025-03-30 18:36:14 +0900

    add: tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  contents: ["./src/**/*.{js,jsx,ts,jsx}"],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        "page-layout": "280px 1fr 280px",
+      },
+    },
+  },
+  plugins: [],
+};
