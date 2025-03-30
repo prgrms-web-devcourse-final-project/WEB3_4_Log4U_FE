60b3f47c0d993c4ce10c9ed16383dcb1329a11d3
JiNookk <ojw0828@naver.com>
2025-03-30 22:30:02 +0900

    add: right side bar

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -13,18 +13,18 @@ function MainContent() {
 export function LeftSideBar() {
   return (
     <SideBar>
-      <h1 className="grow-1 text-text text-4xl">log4U</h1>
+      <h1 className="grow-1 text-4xl">log4U</h1>
       <div className="grow-2">
         <div className="flex items-center mb-2">
           <Image src="/home.png" alt="home image" width={50} height={50} />
           <Link href="/">
-            <span className="text-text p-5 text-2xl font-bold">홈</span>
+            <span className="p-5 text-2xl font-bold">홈</span>
           </Link>
         </div>
         <div className="flex items-center mb-2">
           <Image src="/search.png" alt="search image" width={50} height={50} />
           <Link href="/search">
-            <span className="text-text p-5 text-2xl font-bold">검색</span>
+            <span className="p-5 text-2xl font-bold">검색</span>
           </Link>
         </div>
         <div className="flex items-center mb-2">
@@ -35,13 +35,13 @@ export function LeftSideBar() {
             height={50}
           />
           <Link href="/add-diary">
-            <span className="text-text p-5 text-2xl font-bold">만들기</span>
+            <span className="p-5 text-2xl font-bold">만들기</span>
           </Link>
         </div>
         <div className="flex items-center mb-2">
           <Image src="/mypage.png" alt="mypage image" width={50} height={50} />
           <Link href="/mypage">
-            <span className="text-text p-5 text-2xl font-bold">마이페이지</span>
+            <span className="p-5 text-2xl font-bold">마이페이지</span>
           </Link>
         </div>
       </div>
@@ -49,15 +49,76 @@ export function LeftSideBar() {
   );
 }
 
+export function RightSideBar() {
+  const hotUsers = [
+    "winter",
+    "winter",
+    "winter",
+    "winter",
+    "winter",
+    "winter",
+    "winter",
+    "winter",
+    "winter",
+    "winter",
+    "winter",
+    "winter",
+    "winter",
+    "winter",
+    "winter",
+  ];
+  return (
+    <SideBar>
+      <div className="grow-1">
+        <div className="flex items-center">
+          <Image
+            src={"/test-profile.png"}
+            alt={"profile image"}
+            width={50}
+            height={50}
+          />
+          <span className="p-5 text-2xl font-bold">test user</span>
+        </div>
+        <div>
+          <div className="flex items-center">
+            <Image
+              src={"/sun.png"}
+              alt={"weather image"}
+              width={50}
+              height={50}
+            />
+            <span className="p-5 text-2xl font-bold">6°C</span>
+          </div>
+          <div>서울특별시 강남구</div>
+        </div>
+      </div>
+      <div className="grow-2">
+        <div className="flex items-center">
+          <Image
+            src="/hot-logger.png"
+            alt="hot logger image"
+            width={50}
+            height={50}
+          />
+          <span className={"p-5 text-xl font-bold"}>인기 로거</span>
+        </div>
+        {hotUsers.map((member, index) => (
+          <div key={index} className="pl-4">
+            {index + 1}. {member}
+          </div>
+        ))}
+      </div>
+    </SideBar>
+  );
+}
+
 export default function MyPage() {
   return (
-    <div className="bg-neutral h-screen">
+    <div className="bg-neutral h-screen text-text">
       <div className="bg-neutral h-full w-full grid grid-cols-[280px_1fr_280px] gap-4">
         <LeftSideBar></LeftSideBar>
         <MainContent></MainContent>
-        <SideBar>
-          <p>right sidebar</p>
-        </SideBar>
+        <RightSideBar></RightSideBar>
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
-import SideBar from "@/ui/SideBar";
-import { LeftSideBar } from "@/app/mypage/page";
+import { LeftSideBar, RightSideBar } from "@/app/mypage/page";
 
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div className="h-screen text-text">
       <div className="h-full w-full grid grid-cols-[280px_1fr_280px] gap-4">
         <LeftSideBar></LeftSideBar>
         <div className="justify-items-center content-center border-r border-l border-gray-300">
           <p>home page</p>
         </div>
-        <SideBar>
-          <p>right sidebar</p>
-        </SideBar>
+        <RightSideBar></RightSideBar>
       </div>
     </div>
   );
