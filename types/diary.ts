9f4fd911a88df4db05f3009f74794c4be7626fef
JiNookk <ojw0828@naver.com>
2025-04-05 23:13:16 +0900

    add: diary Type

---
+++ types/diary.ts
@@ -0,0 +1,62 @@
+export namespace Diary {
+  export function isVisibility(value: string): value is Diary.Visibility {
+    return Object.values(Diary.Visibility).includes(value as Diary.Visibility);
+  }
+
+  export function isWeatherType(value: string): value is Diary.WeatherType {
+    return Object.values(Diary.WeatherType).includes(
+      value as Diary.WeatherType,
+    );
+  }
+
+  export const WeatherType = {
+    SUNNY: "SUNNY",
+    CLOUDY: "CLOUDY",
+    RAINY: "RAINY",
+    SNOWY: "SNOWY",
+  } as const;
+  export type WeatherType = (typeof WeatherType)[keyof typeof WeatherType];
+
+  export const Visibility = {
+    PUBLIC: "PUBLIC",
+    PRIVATE: "PRIVATE",
+    FOLLOWER: "FOLLOWER",
+  } as const;
+  export type Visibility = (typeof Visibility)[keyof typeof Visibility];
+
+  export interface DiaryMedia {
+    mediaId: number; // Long
+
+    originalName: string; // String
+
+    contentType: string; // String
+
+    size: number; // Long
+
+    url: string; // String
+
+    orderIndex: number; // Integer
+  }
+
+  export interface Detail {
+    id: number;
+
+    title: string;
+
+    content: string;
+
+    latitude: number;
+
+    longitude: number;
+
+    weatherInfo: WeatherType;
+
+    visibility: Visibility;
+
+    mediaList: DiaryMedia[];
+  }
+
+  export interface CreateDto extends Omit<Diary.Detail, "id"> {}
+
+  export interface Summary {}
+}
