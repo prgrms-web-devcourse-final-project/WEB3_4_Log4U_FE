2e5220bae41e326efca9d4c7763540caabb66db9
JiNookk <ojw0828@naver.com>
2025-04-06 20:24:21 +0900

    fix: use users/me

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 
   useEffect(() => {
     axiosInstance
-      .get("/me")
+      .get("users/me")
       .then((response) => {
         if (response.status !== 200) {
           router.push("/login");
