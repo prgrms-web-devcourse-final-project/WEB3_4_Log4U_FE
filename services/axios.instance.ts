ab0240bb79b400ee727c9460178892b406a4b1e3
JiNookk <ojw0828@naver.com>
2025-04-06 20:22:17 +0900

    add: devtoken

--- services/axios.instance.ts
+++ services/axios.instance.ts
@@ -3,4 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
+  headers: {
+    authorization: `Bearer devtoken`,
+  },
 });
