629c4bf87e2c80e4f44f9bed49865a35738424b3
jingwook <qnfdkf153@gmail.com>
2025-04-11 22:35:57 +0900

    fix: formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import { LeftSideBar, RightSideBar } from "@/ui/SideBar";
-import { Geist, Geist_Mono } from "next/font/google";
-import { usePathname, useRouter } from "next/navigation";∑
-import { useEffect } from "react";
-import { axiosInstance } from "../../services/axios.instance";
+import { LeftSideBar, RightSideBar } from '@/ui/SideBar';
+import { Geist, Geist_Mono } from 'next/font/google';
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { axiosInstance } from '../../services/axios.instance';
 import './globals.css';
 
 const geistSans = Geist({
@@ -27,35 +27,33 @@ export default function RootLayout({
 
   useEffect(() => {
     axiosInstance
-      .get("users/me")
+      .get('users/me')
       .then((response) => {
         if (response.status !== 200) {
-          axiosInstance.get("/oauth2/token/reissue").then((response) => {
+          axiosInstance.get('/oauth2/token/reissue').then((response) => {
             if (response.status !== 200) {
-              router.push("/login");
+              router.push('/login');
             }
           });
         }
       })
       .catch((_error) => {
-        router.push("/login");
+        router.push('/login');
       });
   }, []);
 
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className="bg-neutral h-screen text-text box-border">
-          {pathname === "/login" ? (
-            <div className="h-full w-full grid grid-cols-[280px_1fr_280px] gap-4">
+    <html lang='en'>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <div className='bg-neutral h-screen text-text box-border'>
+          {pathname === '/login' ? (
+            <div className='h-full w-full grid grid-cols-[280px_1fr_280px] gap-4'>
               <div></div>
               {children}
               <div></div>
             </div>
           ) : (
-            <div className="h-full w-full grid grid-cols-[280px_1fr_280px] gap-4">
+            <div className='h-full w-full grid grid-cols-[280px_1fr_280px] gap-4'>
               <LeftSideBar></LeftSideBar>
               {children}
               <RightSideBar></RightSideBar>
