b04f68f558d1ec6606db88b7b9c2b8db4f6472f2
JiNookk <ojw0828@naver.com>
2025-04-15 00:44:16 +0900

    add: comment report and delete api

--- services/report.ts
+++ services/report.ts
@@ -4,10 +4,20 @@ import { Report } from '@root/types/report';
 
 export class ReportService {
   private static readonly DIARY_REPORT_API = (diaryId: string) => `/reports/diaries/${diaryId}`;
+  private static readonly COMMENT_REPORT_API = (commentId: string) =>
+    `/reports/comments/${commentId}`;
 
   static async reportDiary(diaryId: string, body: Report.CreateDto) {
     const response = await axiosInstance.post<void, AxiosResponse<void>, Report.CreateDto>(
       ReportService.DIARY_REPORT_API(diaryId),
+      body
+    );
+    return response.data;
+  }
+
+  static async reportComment(commentId: string, body: Report.CreateDto) {
+    const response = await axiosInstance.post<void, AxiosResponse<void>, Report.CreateDto>(
+      ReportService.COMMENT_REPORT_API(commentId),
       body
     );
     return response.data;
